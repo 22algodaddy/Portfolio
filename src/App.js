3024888fe10e60fb13f69896ccd62b03d0d33bf8
Dhruv Choubey <Choubeydhruv2014@gmail.com>
2022-09-05 21:38:45 +0530

    Completed till About Section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./Components/navbar/navbar.jsx"
+import Home from "./Components/home/home.jsx"
+import About from "./Components/About/about.jsx"
+import Socialconnect from "./Components/SocialLinks/socialLink.jsx"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Home />
+      <About></About>
+      <Socialconnect></Socialconnect>
     </div>
   );
 }
